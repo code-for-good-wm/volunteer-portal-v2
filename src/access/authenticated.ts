416a0bc8cb573870b24808de5d193c1379577934
Sloth I <github@slothwerks.com>
2024-10-25 11:20:13 -0400

    Tweaks to Users config

--- src/access/authenticated.ts
+++ src/access/authenticated.ts
@@ -4,6 +4,7 @@ import type { User } from '../../payload-types'
 
 type isAuthenticated = (args: AccessArgs<User>) => boolean
 
+// NOTE: In a CollectionConfig, isAuthenticated is a default for CRUD operations
 export const authenticated: isAuthenticated = ({ req: { user } }) => {
   return Boolean(user)
 }
--- src/collections/Users/index.ts
+++ src/collections/Users/index.ts
@@ -1,23 +1,33 @@
-import type { CollectionConfig } from 'payload'
+import type { Access, CollectionConfig, User } from 'payload'
 
-import { authenticated } from '../../access/authenticated'
-import { isAdminOrSelf } from '../../access/adminOrSelf'
+export const isAdminOrSelf: Access<User> = ({ id, req: { user } }) => {
+  // Need to be logged in
+  if (user) {
+    if (user.role?.includes('admin')) {
+      return true
+    }
+    // If any other type of user, only provide access to themselves
+    // TODO: is the document ID of the users collection the same as the user ID?
+    return id === user.id
+  }
+  // Reject everyone else
+  return false
+}
 
 const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
   access: {
-    admin: isAdminOrSelf,
-    create: authenticated,
-    delete: authenticated,
-    read: authenticated,
-    update: authenticated,
+    delete: isAdminOrSelf,
+    read: isAdminOrSelf,
+    update: isAdminOrSelf,
   },
   fields: [
     {
       name: 'name',
       type: 'text',
       required: true,
+      // TODO: min / max length?
     },
     {
       name: 'role',
